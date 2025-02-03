6882659ddc8e17b1ea521ac49f228e78cda0e89d
Chavela <angie87wong@gmail.com>
2025-02-03 17:15:33 -0300

    Agrego imagen animada en el index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,32 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import Pricing from "../components/Pricing";
 import Footer from "../components/Footer";
-import Layout from "../components/Layout"; // Importamos Layout
+import Layout from "../components/Layout"; 
 
 export default function Home() {
   return (
     <Layout>
       <Navbar />
       <h1 className="text-center text-3xl font-sans mt-10 text-white-900">
-      <br />
+      <br /> <br /> <br />
         <span className="font-bold text-4xl">
-          Ingeniería en Informática | Gestión de Proyectos IT | Desarrollo de Software | Product Owner
+          Ingeniería en Informática | Gestión de Proyectos IT | Product Owner | Desarrollo de Software 
         </span>
-        <br /> <br /> <br />
-        Soy Product Owner y estudiante de 4to año de Ingeniería en Informática en la UBA, 
-        con una gran pasión por la gestión de proyectos tecnológicos y la dirección de equipos. 
-        Mi enfoque está en traducir las necesidades de negocio en soluciones estratégicas, 
-        optimizando procesos y maximizando el impacto de los productos digitales.
+        <img
+          src="/Logo.png" // Ruta de la imagen
+          alt="Descripción de la imagen"
+          className=" mx-auto mb-8 w-[800px] h-auto animate-move-vertical" 
+          
+        />
+        <br /> 
+        Soy Product Owner y estudiante avanzada de Ingeniería en Informática en la UBA, 
+        con una gran pasión por la gestión de proyectos tecnológicos y el trabajo en equipo. 
+        Me especializo en ayudar a empresas y a estudiantes que recién comienzan en el mundo 
+        de la informática, optimizando procesos y gestionando proyectos tecnológicos de manera 
+        eficiente. Mi enfoque está en entender las necesidades del negocio y transformar esas 
+        ideas en soluciones claras y efectivas, maximizando el impacto de los productos digitales. 
+        Si estás buscando mejorar o emprender proyectos en el ámbito IT, ¡puedo ayudarte!
+        <br /> <br /> <br /> <br /> <br /> <br /> 
       </h1>
       <Hero />
       <Features />
