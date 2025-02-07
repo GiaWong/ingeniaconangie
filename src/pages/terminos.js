3edde38d135ad82ecf76b858b6c3a7183284c024
Chavela <angie87wong@gmail.com>
2025-02-07 19:14:45 -0300

    feat: mejoro color de fondo de la pagina terminos.js

--- src/pages/terminos.js
+++ src/pages/terminos.js
@@ -3,17 +3,19 @@ import Layout from "../components/Layout";
 export default function Terminos() {
   return (
     <Layout>
-      <div className="max-w-4xl mx-auto p-8 text-gray-200">
-        <h1 className="text-3xl font-bold text-center mb-6">Términos y Condiciones</h1>
-        <p>
-          Todo el contenido, diseño y estructura de esta página web es propiedad exclusiva de 
-          IngeniaconAngie y está prohibida su reproducción total o parcial sin autorización.
-        </p>
-        <p className="mt-4">
-          El sitio web se ofrece únicamente con fines informativos. Cualquier intento de hacking, 
-          sabotaje o ataque informático contra la plataforma será denunciado de acuerdo con 
-          la legislación vigente en Argentina.
-        </p>
+      <div className="min-h-screen bg-transparent text-gray-200">
+        <div className="max-w-4xl mx-auto p-8">
+          <h1 className="text-3xl font-bold text-center mb-6">Términos y Condiciones</h1>
+          <p>
+            Todo el contenido, diseño y estructura de esta página web es propiedad exclusiva de 
+            ingeniaconangie y está prohibida su reproducción total o parcial sin autorización.
+          </p>
+          <p className="mt-4">
+            El sitio web se ofrece únicamente con fines informativos. Cualquier intento de hacking, 
+            sabotaje o ataque informático contra la plataforma será denunciado de acuerdo con 
+            la legislación vigente en Argentina.
+          </p>
+        </div>
       </div>
     </Layout>
   );
