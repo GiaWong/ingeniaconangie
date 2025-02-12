38aeedc12ee3b8edb8515a32f6092996c0ed42d4
Chavela <angie87wong@gmail.com>
2025-02-12 19:03:23 -0300

    fix: corrijo dimensiones

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen w-full bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-600 text-white animate-fade-in flex flex-col">
+    <div className="min-h-screen w-full bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-600 text-white animate-fade-in flex flex-col justify-start">
       {children}
     </div>
   );
